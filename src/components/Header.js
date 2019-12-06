69249b259545c1ab04d962a0a3393ffa8566ab48
r4bb1t <danettofficial@gmail.com>
2019-12-06 17:12:42 +0900

    header modify

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
     return (
         <div className="header" style={HeaderStyle}>
             <span style={HeaderContentStyle}>
-                <span style={LogoStyle}>LOGO</span>
-                <span style={LinkStyle}>
+                <div style={LogoStyle}>LOGO</div>
+                <div style={LinkStyle}>
                     <NavLink to="Home" className="aLink" activeClassName="activeLink">HOME</NavLink>
                     <NavLink to="Page1" className="aLink" activeClassName="activeLink">PAGE1</NavLink>
                     <NavLink to="Page2" className="aLink" activeClassName="activeLink">PAGE2</NavLink>
-                </span>
+                </div>
             </span>
             <div style={{height: "2px", background: "linear-gradient(to right, palevioletred, mediumpurple)"}}/>
         </div>
@@ -22,31 +22,31 @@ const HeaderStyle = {
     fontFamily: 'nanumsquare',
     position: "fixed",
     top: "0px",
+    left: "0px",
     width: "100%",
-    backgroundColor: "white",
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
     color: "black",
     boxShadow: "0 5px 5px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
 }
 
 const HeaderContentStyle = {
-    display: "flex",
     width: "100%",
     flexDirection: "row",
-    alignItems: "center",
+    textAlign: "center",
     padding: "0.3rem 5rem"
 }
 
 const LogoStyle = {
+    fontFamily: 'nanumsquare',
     fontSize: "2.0rem",
     fontWeight:"700",
     color: "palevioletred"
 }
 
 const LinkStyle = {
+    marginTop: "1.0rem",
     fontSize: "1.3rem",
     fontWeight:"400",
-    position: "fixed",
-    right: "5rem",
 }
 
 export default Header;
