f7402aae2c43f6ed67d1ad9c2d45343df91ed5b3
r4bb1t <danettofficial@gmail.com>
2019-12-13 03:11:47 +0900

    Updates

---
+++ precache-manifest.6f8fbb707ae5a2a3ad3eef3c022cd916.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b540f16b65029189c19b6d55e085454",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "71020d21953daf1d8367",
+    "url": "/react-practice/static/css/2.418ca425.chunk.css"
+  },
+  {
+    "revision": "98f6da3d9a10a289ebfc",
+    "url": "/react-practice/static/css/main.e4679b9b.chunk.css"
+  },
+  {
+    "revision": "71020d21953daf1d8367",
+    "url": "/react-practice/static/js/2.826e494c.chunk.js"
+  },
+  {
+    "revision": "0d100b80824825291aff6079ccd9adc7",
+    "url": "/react-practice/static/js/2.826e494c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98f6da3d9a10a289ebfc",
+    "url": "/react-practice/static/js/main.b222c556.chunk.js"
+  },
+  {
+    "revision": "7639ba1100d91d7262a5",
+    "url": "/react-practice/static/js/runtime-main.0011e385.js"
+  },
+  {
+    "revision": "7059983735a5faa8ac6c0bae126f8a11",
+    "url": "/react-practice/static/media/logo_facebook.70599837.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/react-practice/static/media/logo_instagram.0e84d592.png"
+  },
+  {
+    "revision": "d00d31463f988a3d6394218c794d4e1b",
+    "url": "/react-practice/static/media/main.d00d3146.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-practice/precache-manifest.42d9856b760c2ffd987f34c118d4bbfb.js"
+  "/react-practice/precache-manifest.6f8fbb707ae5a2a3ad3eef3c022cd916.js"
 );
 
 self.addEventListener('message', (event) => {
